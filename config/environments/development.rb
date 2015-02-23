edb1d62de31b63347e26c75256062422c6b7f708
ThomasConnolly <tom.connolly@comcast.net>
2015-02-23 10:52:58 -0500

    working on email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,14 +28,16 @@ Kateco::Application.configure do
 
 config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 config.action_mailer.raise_delivery_errors = true
+config.action_mailer.perform_deliveries = true
 config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
+ActionMailer::Base.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
   domain: ENV["GMAIL_DOMAIN"],
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  password: ENV["GMAIL_PASSWORD"],
+  openssl_verify_mode: "none"
   }
 end
